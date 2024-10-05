88ee8abb5c8bca7da6f9c8dc2086d66be3783d65
WJQSERVER <wjqserver@outlook.com>
2024-10-06 01:43:24 +0800

    fix

--- init.sh
+++ init.sh
@@ -10,8 +10,8 @@ if [ ! -f /data/${APPLICATON}/config/blacklist.yaml ]; then
     cp /data/${APPLICATON}/blacklist.yaml /data/${APPLICATON}/config/blacklist.yaml
 fi
 
-if [ ! -f /data/${APPLICATON}/config/config.yaml ]; then
-    cp /data/${APPLICATON}/config.yaml /data/${APPLICATON}/config/config.yaml
+if [ ! -f /data/${APPLICATON}/config/config.json ]; then
+    cp /data/${APPLICATON}/config.json /data/${APPLICATON}/config/config.json
 fi
 
 /data/caddy/caddy run --config /data/caddy/config/Caddyfile > /data/${APPLICATON}/log/caddy.log 2>&1 &
