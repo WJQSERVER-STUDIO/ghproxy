36fe815e35e40195558d4427a1c0df4659ecb1ea
WJQSERVER <114663932+WJQSERVER@users.noreply.github.com>
2025-01-20 10:53:05 +0800

    v2.0.1 (#34)

--- docker/dockerfile/release/init.sh
+++ docker/dockerfile/release/init.sh
@@ -2,10 +2,6 @@
 
 APPLICATION=ghproxy
 
-if [ ! -f /data/caddy/config/Caddyfile ]; then
-    cp /data/caddy/Caddyfile /data/caddy/config/Caddyfile
-fi
-
 if [ ! -f /data/${APPLICATION}/config/blacklist.json ]; then
     cp /data/${APPLICATION}/blacklist.json /data/${APPLICATION}/config/blacklist.json
 fi
@@ -17,8 +13,6 @@ fi
 if [ ! -f /data/${APPLICATION}/config/config.toml ]; then
     cp /data/${APPLICATION}/config.toml /data/${APPLICATION}/config/config.toml
 fi
-
-/data/caddy/caddy run --config /data/caddy/config/Caddyfile > /data/${APPLICATION}/log/caddy.log 2>&1 &
 
 /data/${APPLICATION}/${APPLICATION} -cfg /data/${APPLICATION}/config/config.toml > /data/${APPLICATION}/log/run.log 2>&1 &
 
--- init.sh
+++ init.sh
@@ -2,10 +2,6 @@
 
 APPLICATON=ghproxy
 
-if [ ! -f /data/caddy/config/Caddyfile ]; then
-    cp /data/caddy/Caddyfile /data/caddy/config/Caddyfile
-fi
-
 if [ ! -f /data/${APPLICATON}/config/blacklist.json ]; then
     cp /data/${APPLICATON}/blacklist.json /data/${APPLICATON}/config/blacklist.json
 fi
@@ -17,8 +13,6 @@ fi
 if [ ! -f /data/${APPLICATON}/config/config.yaml ]; then
     cp /data/${APPLICATON}/config.yaml /data/${APPLICATON}/config/config.yaml
 fi
-
-/data/caddy/caddy run --config /data/caddy/config/Caddyfile > /data/${APPLICATON}/log/caddy.log 2>&1 &
 
 /data/${APPLICATON}/${APPLICATON} > /data/${APPLICATON}/log/run.log 2>&1 &
 
